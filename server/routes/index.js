db2444f2127078127f415fdd63a35d0a4154733d
Juliane-C <juliane.cristinara97@gmail.com>
2021-03-27 20:31:32 -0300

    criaçao das rotas de usuarios

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,11 @@
 const { Router } = require('express');
-const ExampleRouter = require('./ExampleRouter');
+const ExampleRouter = require('./usersHB');
 
 const router = Router();
 
 // aqui vai todas as rotas
 router.use('/exemple', ExampleRouter);
+
+router.use('/users', ExampleRouter);
 
 module.exports = router;
