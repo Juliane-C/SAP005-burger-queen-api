c41138480e2740297a5872cba699ee39f17da85e
Juliane-C <juliane.cristinara97@gmail.com>
2021-03-29 22:49:31 -0300

    fix/correçao da associaçao de chave estrangeira

--- server/db/models/orders.js
+++ server/db/models/orders.js
@@ -2,24 +2,14 @@ const { Model } = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
   class Orders extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      Orders.belongsTo(models.Users, {
+        foreignKey: 'id_user',
+      });
     }
   }
   Orders.init(
     {
-      id_user: {
-        type: DataTypes.INTEGER,
-        references: {
-          model: 'Users',
-          key: 'id',
-        },
-      },
       client_name: DataTypes.STRING,
       table: DataTypes.INTEGER,
       status: DataTypes.STRING,
